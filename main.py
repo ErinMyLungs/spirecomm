0d3e0e3aedc52b08e76cfb0926f3d600cd4db19e
Erin <ErinLWeir@gmail.com>
2019-11-06 18:15:56 -0800

    Edit the __main__ to standardize run with seed and class

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from spirecomm.spire.character import PlayerClass
 
 
 if __name__ == "__main__":
-    agent = SimpleAgent()
+    train_class = PlayerClass.IRONCLAD
+    seed = '3BLZA4F5DPM6P'
+    agent = SimpleAgent(chosen_class=train_class)
     coordinator = Coordinator()
     coordinator.signal_ready()
     coordinator.register_command_error_callback(agent.handle_error)
     coordinator.register_state_change_callback(agent.get_next_action_in_game)
     coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
 
-    # Play games forever, cycling through the various classes
-    for chosen_class in itertools.cycle(PlayerClass):
-        agent.change_class(chosen_class)
-        result = coordinator.play_one_game(chosen_class)
+    result = coordinator.play_one_game(player_class=train_class, ascension_level=0, seed=seed)
