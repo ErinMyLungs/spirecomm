8b7523d47669ff29c6d2abd7fc5695e30f90aa0e
Erin <ErinLWeir@gmail.com>
2019-11-07 22:18:18 -0800

    Deck update should now properly update and main.py now will cycle through a seed list

--- main.py
+++ main.py
@@ -8,12 +8,31 @@ from spirecomm.spire.character import PlayerClass
 
 
 if __name__ == "__main__":
+    solo = sys.argv[1]
+
     train_class = PlayerClass.IRONCLAD
-    seed = '3BLZA4F5DPM6P'
+    seed_list = [
+        "3BLZA4F5DPM6P",
+        "TKUZHLYGTK6B6",
+        "MKMDE5BHDADPS",
+        "5DWNQJBD5BPGM",
+        "8O5SXDOKOJTT4",
+        "JHHEA7KNEOGKI",
+        "LI7X71KSALAGR",
+        "53HJXL2N4CEYI",
+        "E4BMONWA67GME",
+        "LNYDX9VNZYATK",
+    ]
+
+    if solo:
+        seed_list = [seed_list[0]]
+
     timestamp = str(int(time.time()))
 
+    agent = SimpleAgent(
+        chosen_class=train_class, use_default_drafter=False, timestamp=timestamp
+    )
 
-    agent = SimpleAgent(chosen_class=train_class, use_default_drafter=False, timestamp=timestamp)
     agent.drafter.dump_weights(timestamp)
     coordinator = Coordinator()
     coordinator.signal_ready()
@@ -21,4 +40,7 @@ if __name__ == "__main__":
     coordinator.register_state_change_callback(agent.get_next_action_in_game)
     coordinator.register_out_of_game_callback(agent.get_next_action_out_of_game)
 
-    result = coordinator.play_one_game(player_class=train_class, ascension_level=0, seed=seed)
+    for seed in seed_list:
+        result = coordinator.play_one_game(
+            player_class=train_class, ascension_level=0, seed=seed
+        )
